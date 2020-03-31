ef17ffb98e66bb3e374e2dac333ae22b3ea1e356
AkimbaevTimyr <akimbaev.tim.2002@gmail.com>
2020-03-31 09:13:46 +0300

    Changed the HTML for the site.

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -8,6 +8,11 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
+project_home = u'/home/akimbaevtimyr/mysite'
+if project_home not is sys.path:
+	sys.path.append(project_home)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'myproject.settings'
 
 from django.core.wsgi import get_wsgi_application
 
